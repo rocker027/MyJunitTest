3fdafea4f516e97d4087c1b4db991c24f89f2795
rocker <coorsrayh@gmail.com>
2020-03-23 18:21:53 +0800

    feat : Android Koin DI
    DI ShareViewModel & ServiceApi & AppDatabase

--- app/src/main/java/com/example/myjunittest/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/myjunittest/ui/dashboard/DashboardFragment.kt
@@ -6,13 +6,17 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.myjunittest.R
+import com.example.myjunittest.api.ServiceApi
 import com.example.myjunittest.ui.home.HomeRepository
 import com.example.myjunittest.ui.home.ProductAPI
+import io.reactivex.functions.Consumer
+import io.reactivex.schedulers.Schedulers
+import org.koin.android.ext.android.inject
 
 class DashboardFragment : Fragment() {
 
     private lateinit var dashboardViewModel: DashboardViewModel
-
+    val serviceAPI by inject<ServiceApi>()
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -20,6 +24,9 @@ class DashboardFragment : Fragment() {
     ): View? {
         val homeRepository = HomeRepository(ProductAPI())
         dashboardViewModel = DashboardViewModel(homeRepository)
+        serviceAPI.getProduct()
+            .subscribeOn(Schedulers.io())
+            .subscribe({ t ->  println(t.body()) })
         val root = inflater.inflate(R.layout.fragment_dashboard, container, false)
         return root
     }
