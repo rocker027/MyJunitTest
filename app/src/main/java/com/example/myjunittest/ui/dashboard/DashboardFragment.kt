eae3e73fd7e06ddc66c367ff35937a282969689e
rocker <coorsrayh@gmail.com>
2020-05-07 14:39:38 +0800

    feature : scartlet websocket
    目前測試可以接到API

--- app/src/main/java/com/example/myjunittest/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/myjunittest/ui/dashboard/DashboardFragment.kt
@@ -10,14 +10,25 @@ import com.example.myjunittest.R
 import com.example.myjunittest.api.ServiceApi
 import com.example.myjunittest.ui.home.HomeRepository
 import com.example.myjunittest.ui.home.ProductAPI
-import com.neovisionaries.ws.client.WebSocket
-import com.neovisionaries.ws.client.WebSocketAdapter
-import com.neovisionaries.ws.client.WebSocketFactory
-import com.neovisionaries.ws.client.WebSocketFrame
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+import com.tinder.scarlet.Scarlet
+import com.tinder.scarlet.WebSocket
+import com.tinder.scarlet.lifecycle.android.AndroidLifecycle
+import com.tinder.scarlet.messageadapter.moshi.MoshiMessageAdapter
+import com.tinder.scarlet.retry.ExponentialBackoffStrategy
+import com.tinder.scarlet.streamadapter.rxjava2.RxJava2StreamAdapterFactory
+import com.tinder.scarlet.websocket.okhttp.newWebSocketFactory
+import com.tinder.scarlet.ws.Receive
+import com.tinder.scarlet.ws.Send
+import io.reactivex.Flowable
 import io.reactivex.Observable
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.fragment_dashboard.view.*
+import okhttp3.OkHttpClient
 import org.koin.android.ext.android.inject
+import timber.log.Timber
+import java.util.concurrent.TimeUnit
 
 class DashboardFragment : Fragment() {
 
@@ -58,27 +69,100 @@ class DashboardFragment : Fragment() {
     //
     private fun callWebSocketV2() {
         // Create a WebSocket with a socket connection timeout value.
-        val ws = WebSocketFactory().createSocket(
-            "ws://10.200.252.185:17803/register-mobile?token=da9a7a0c-968d-4aed-86c1-4f2dda2dc193",
-            5000
+//        val BASE_URL = "wss://echo.websocket.org"
+        val BASE_URL = "ws://10.200.252.185:17803/register-mobile?token=da9a7a0c-968d-4aed-86c1-4f2dda2dc193"
+
+
+        val okHttpClient =
+            OkHttpClient.Builder()
+                .connectTimeout(10, TimeUnit.SECONDS)
+                .readTimeout(10, TimeUnit.SECONDS)
+                .writeTimeout(10, TimeUnit.SECONDS)
+                .build()
+
+        val scarlet = Scarlet.Builder()
+            // If the connection fails how and when should we retry to reconnect? -> backoffStrategy
+            .backoffStrategy(ExponentialBackoffStrategy(2000, 4000))
+            // We can declare our adapter: it adapts our custom messages to Message.
+            // In this app I use Msg (my custom simple class), just as an example.
+            .addMessageAdapterFactory(MoshiMessageAdapter.Factory())
+            // We can declare our adapter: it adapts our custom streams to Stream
+            // In this app I use Flowable, just as an example.
+            .addStreamAdapterFactory(RxJava2StreamAdapterFactory())
+            // This actually creates the WebSocket: it is the only compulsory parameter!
+            .webSocketFactory(okHttpClient.newWebSocketFactory(BASE_URL))
+            // WebSocket will be automatically closed when app is not in foreground. We could combine it
+            // with other life cycles to stop the WebSocket connection at some point (e.g. when a user logs out)
+            // I have put an example of in the lifecycle package, even though I don't use it in this app. (feel free to try it)
+//            .lifecycle(AndroidLifecycle.ofApplicationForeground(activity!!.application))
+            .build()
+
+        val socketService = scarlet.create(SocketService::class.java)
+
+//        socketService.sendText(
+//            Msg(
+//                "test"
+//            )
+//        )
+
+        socketService.observeWebSocketEvent().subscribe(
+            {
+                println("${it}")
+                if (it is WebSocket.Event.OnConnectionOpened<*>) {
+                    println("ready to send")
+                    socketService.sendText(Msg("test"))
+                }
+            }, { e ->
+                println("${e.message}")
+
+            }
         )
 
-        // Register a listener to receive WebSocket events.
-        ws.addListener(object : WebSocketAdapter() {
-            override fun onTextMessage(websocket: WebSocket?, text: String?) {
-                super.onTextMessage(websocket, text)
-                Log.v("WSS", text)
+        socketService.observeText().subscribe(
+            { println(it.message) }, { e ->
+                println("${e.message}")
             }
+        )
 
-            override fun onCloseFrame(websocket: WebSocket?, frame: WebSocketFrame?) {
-                super.onCloseFrame(websocket, frame)
-                Log.v("WSS", "closing socket")
-            }
-        })
+//        socketService.observeWebSocketEvent().map{
+//            when(it){
+//                is WebSocket.Event.OnConnectionOpened<*> -> Timber.d("Connection opened")
+//                is WebSocket.Event.OnMessageReceived -> Timber.d("Message received")
+//                is WebSocket.Event.OnConnectionClosing -> Timber.d("Connection closing")
+//                is WebSocket.Event.OnConnectionClosed -> Timber.d("Connection closed")
+//                is WebSocket.Event.OnConnectionFailed -> Timber.d("Connection failed")
+//            }
+//            (it is WebSocket.Event.OnConnectionOpened<*> || it is WebSocket.Event.OnMessageReceived)
+//        }
+//
+//        socketService.observeWebSocketEvent()
+//            .subscribe {
+//                when(it){
+//                    is WebSocket.Event.OnConnectionOpened<*> -> Timber.d("Connection opened")
+//                    is WebSocket.Event.OnMessageReceived -> Timber.d("Message received")
+//                    is WebSocket.Event.OnConnectionClosing -> Timber.d("Connection closing")
+//                    is WebSocket.Event.OnConnectionClosed -> Timber.d("Connection closed")
+//                    is WebSocket.Event.OnConnectionFailed -> Timber.d("Connection failed")
+//                }
+//            }
 
-        ws.connect()
-        ws.sendText("hello !")
-        ws.disconnect()
     }
 
 }
+
+interface SocketService {
+    @Receive
+    fun observeText(): Flowable<Msg>
+
+    @Send
+    fun sendText(text: Msg)
+
+    @Receive
+    fun observeWebSocketEvent():
+            Flowable<WebSocket.Event>
+}
+
+@JsonClass(generateAdapter = true)
+data class Msg(
+    @Json(name = "message") var message: String = ""
+)
