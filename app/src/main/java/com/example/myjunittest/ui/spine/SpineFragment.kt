ddb5ea0c2eea8e8c6af847d76ad11b67edba4950
rocker <coorsrayh@gmail.com>
2020-05-11 16:12:07 +0800

    construction :  新增spine fragment
    1. 在此fragment 點選按鈕，顯示spine動畫
    2. 於register 增加 btn , 點選後跳轉至spine fragment
    3. 跳轉行為用 navigation方式

--- app/src/main/java/com/example/myjunittest/ui/spine/SpineFragment.kt
+++ app/src/main/java/com/example/myjunittest/ui/spine/SpineFragment.kt
@@ -1,11 +1,14 @@
-package com.example.myjunittest
+package com.example.myjunittest.ui.spine
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.badlogic.gdx.graphics.OrthographicCamera
+import com.example.myjunittest.R
+import kotlinx.android.synthetic.main.fragment_spine.*
 
 /**
  * A simple [Fragment] subclass.
@@ -25,5 +28,13 @@ class SpineFragment : Fragment() {
 
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        button.setOnClickListener {
+            val intent = Intent(activity, HumanOidActivity::class.java)
+            startActivity(intent)
+
+        }
+    }
 }
