ad61dc724627bc24d21132f8de95e1cece5a1ab0
rocker <coorsrayh@gmail.com>
2020-05-22 15:48:51 +0800

    refactor：因應資料顯示調整畫面，以及Fragment內的邏輯
    狀況敘述：
    調整LiveData為UiStatus，對應不同行為，作不同畫面顯示更新，改使用coroutines去作執行續切換
    
    調整項目：
    1. fragment_ec_pading_animation - 調整畫面顯示，新增title、desc、updateTime
    2. EcPagingAnimationFragment - 新增 UiStatus (Sealed class) 來定義畫面處理行為，將調整LivaData為UiStatus，並在傳遞時，使用coroutines

--- app/src/main/java/com/example/myjunittest/ui/menu/animation/EcPagingAnimationFragment.kt
+++ app/src/main/java/com/example/myjunittest/ui/menu/animation/EcPagingAnimationFragment.kt
@@ -5,65 +5,117 @@ import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import android.widget.TextView
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.PagerSnapHelper
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.widget.ViewPager2
 import com.example.myjunittest.R
+import com.example.myjunittest.api.GankData
+import com.example.myjunittest.base.BaseFragment
 import com.example.myjunittest.utils.layoutmanager.CircleLayoutManager
+import kotlinx.coroutines.launch
 
 
-class EcPagingAnimationFragment : Fragment() {
-
-    companion object {
-        fun newInstance() =
-            EcPagingAnimationFragment()
-    }
-
+class EcPagingAnimationFragment : BaseFragment() {
     private lateinit var viewModel: EcPagingAnimationViewModel
-
-    // find view
     private lateinit var viewpager: ViewPager2
     private lateinit var recyclerView: RecyclerView
+    private lateinit var tvTitle: TextView
+    private lateinit var tvUpdateTime: TextView
+    private lateinit var tvDesc: TextView
+    private lateinit var swipItemsAdapter: SwipItemsAdapter
+    private lateinit var topImagePagerAdapter: TopImagePagerAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         viewModel = ViewModelProvider(this).get(EcPagingAnimationViewModel::class.java)
-        setHasOptionsMenu(true)
         val view = inflater.inflate(R.layout.fragment_ec_paging_animation, container, false)
+        setHasOptionsMenu(true)
+        fetchGankApiData()
         initView(view)
         initLiveData()
         setUpComponents()
         return view
     }
 
+    private fun fetchGankApiData() {
+        viewModel.callGankApiData()
+    }
+
+    sealed class UiStatus {
+        /**
+         * 更新當前item位置
+         *
+         * @property pos index
+         */
+        data class UpdateCurrentPosititon(val pos: Int) : UiStatus()
+
+        /**
+         * call api完成更新adapter
+         */
+        object UpdateGankData : UiStatus()
+    }
+
+    /**
+     * 初始化 live data
+     */
     private fun initLiveData() {
-        viewModel.currentPositionLiveData.observe(viewLifecycleOwner, Observer { pos ->
-            viewpager.currentItem = pos
-            recyclerView.smoothScrollToPosition(pos)
-        })
+        viewModel.fragmentLiveData.observe(
+            viewLifecycleOwner,
+            Observer { uiState ->
+                val newData = viewModel.gankDatas
+                when (uiState) {
+                    is UiStatus.UpdateCurrentPosititon -> {
+                        viewpager.currentItem = uiState.pos
+                        recyclerView.smoothScrollToPosition(uiState.pos)
+
+                        newData[uiState.pos].run {
+                            tvTitle.text = title
+                            tvDesc.text = desc
+                            tvUpdateTime.text = createdAt
+                        }
+                    }
+                    is UiStatus.UpdateGankData -> {
+                        swipItemsAdapter.setNewData(newData)
+                        topImagePagerAdapter.setNewData(newData)
+                    }
+                }
+            })
 
     }
 
+    /**
+     * 初始化 View Components
+     *
+     * @param view
+     */
     private fun initView(view: View) {
         viewpager = view.findViewById(R.id.vp_imagepager)
         recyclerView = view.findViewById(R.id.rv_items)
+        tvTitle = view.findViewById(R.id.tv_title)
+        tvUpdateTime = view.findViewById(R.id.tv_update_time)
+        tvDesc = view.findViewById(R.id.tv_desc)
     }
 
     private fun setUpComponents() {
         // 初始化Viewpager
-        val data = viewModel.fakeItems
-        viewpager.adapter = TopImagePagerAdapter(data)
+        val data = mutableListOf<GankData>()
+        topImagePagerAdapter = TopImagePagerAdapter(data)
+        viewpager.adapter = topImagePagerAdapter
 
         // 初始化RecyclerView
         recyclerView.setHasFixedSize(true)
         val circleLayoutManager = CircleLayoutManager(requireContext())
         recyclerView.layoutManager = circleLayoutManager
-        recyclerView.adapter = SwipItemsAdapter(data)
+        swipItemsAdapter = SwipItemsAdapter(data)
+        recyclerView.adapter = swipItemsAdapter
+        // 讓recyclerview 有 pager效果
+        PagerSnapHelper().attachToRecyclerView(recyclerView)
 
         // recyclerView scroll狀態
         recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
@@ -71,7 +123,10 @@ class EcPagingAnimationFragment : Fragment() {
                 super.onScrollStateChanged(rv, newState)
                 if (newState == RecyclerView.SCROLL_STATE_IDLE) {
                     val currentPosition = (rv.layoutManager as CircleLayoutManager).currentPosition
-                    viewModel.currentPositionLiveData.value = currentPosition
+                    mainScope.launch {
+                        viewModel.fragmentLiveData.value =
+                            UiStatus.UpdateCurrentPosititon(currentPosition)
+                    }
                 }
             }
         })
@@ -80,7 +135,10 @@ class EcPagingAnimationFragment : Fragment() {
         viewpager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
             override fun onPageSelected(position: Int) {
                 super.onPageSelected(position)
-                viewModel.currentPositionLiveData.value = position
+                mainScope.launch {
+                    viewModel.fragmentLiveData.value =
+                        UiStatus.UpdateCurrentPosititon(position)
+                }
             }
         })
     }
@@ -96,4 +154,3 @@ class EcPagingAnimationFragment : Fragment() {
         return super.onOptionsItemSelected(item)
     }
 }
-
